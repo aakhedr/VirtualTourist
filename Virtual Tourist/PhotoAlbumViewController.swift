f19f6b2095c0070043bfcca39981e42a8507a6c3
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2015-08-01 13:27:00 +0200

    Add fetchedResultsController to PhotoAlbumViewController - Problem with sectionInfo = nil

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import MapKit
+import CoreData
 
-class PhotoAlbumViewController: UIViewController {
+class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, NSFetchedResultsControllerDelegate {
 
-    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var photoCollectionView: UICollectionView!
     
     var tabbedPin: Pin!
+    
+    private var sharedContext : NSManagedObjectContext {
+        return CoreDataStackManager.sharedInstance().managedObjectContext!
+    }
+    
+    lazy private var fetchedResultsController : NSFetchedResultsController = {
+        let fetchRequest = NSFetchRequest(entityName: "Photo")
+        
+        // TODO: Change key as you add new properties to Photo
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "imageURL", ascending: true)]
+        fetchRequest.predicate = NSPredicate(format: "pin == %@", self.tabbedPin);
+        
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+            managedObjectContext: self.sharedContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil)
+        
+        return fetchedResultsController
+    }()
     
     // MARK:- View lifecycle
     
@@ -32,11 +53,72 @@ class PhotoAlbumViewController: UIViewController {
             tabbedPin.lat as! CLLocationDegrees,
             tabbedPin.lon as! CLLocationDegrees
         )
-        let span = MKCoordinateSpanMake(0.5, 0.5)
+        let span = MKCoordinateSpanMake(1.0, 1.0)
         mapView.region = MKCoordinateRegionMake(center, span)
+        
+        // Set photoCollectionView delegate and data srouce
+        photoCollectionView.delegate = self
+        photoCollectionView.dataSource = self
+        
+        // Set fetchedResultsController delegate 
+        fetchedResultsController.delegate = self
     }
     
-    // MARK:- Helpers
+    // MARK: - Collection View Data Source
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let sectionInfo = fetchedResultsController.sections![section] as! NSFetchedResultsSectionInfo
+        return sectionInfo.numberOfObjects
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let photo = fetchedResultsController.objectAtIndexPath(indexPath) as! Photo
+        let identifier = "photoCell"
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath) as! UICollectionViewCell
+
+        return cell
+    }
+    
+    // MARK: - Fetched Results Controller Delegate
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        println("controllerWillChangeContent in PhotoAlbumViewController called")
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        switch type {
+
+        case .Insert:
+            photoCollectionView.insertSections(NSIndexSet(index: sectionIndex))
+            
+        case .Delete:
+            photoCollectionView.insertSections(NSIndexSet(index: sectionIndex))
+
+        default:
+            return
+        }
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        switch type {
+
+        case .Insert:
+            photoCollectionView.insertItemsAtIndexPaths([newIndexPath!])
+            
+        case .Delete:
+            photoCollectionView.deleteItemsAtIndexPaths([indexPath!])
+            
+        default:
+            return
+        }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        println("controllerDidChangeContent in PhotoAlbumViewController called")
+    }
+    
+    // MARK: - Helpers
     
 
 }
